d40ffebbe662ebe3349a9de572146e1b2b820169
AdanP <adan.perez@cua.uam.mx>
2025-06-13 13:52:29 -0600

    Se cambio de 0.0.0.0 a localhost

--- pub_subs/broker.py
+++ pub_subs/broker.py
@@ -24,7 +24,7 @@ def handle_client(conn, addr):
         print(f"[!] Error con {addr}: {e}")
     finally:
         conn.close()
-def start_broker(hots = '0,0,0,0', port = 14000):
+def start_broker(hots = 'localhost', port = 14000):
     server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     server.bind((hots, port))
     server.listen(5)
